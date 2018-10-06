7618542ef0bdce51df7e42a37e288fec4eb2ebf2
Achaad <perant@zoho.com>
2018-10-06 17:33:36 +0300

    Created main methods of the rotor class

--- src/antenna_rotation_node.py
+++ src/antenna_rotation_node.py
@@ -24,9 +24,34 @@ class Rotor:
         self._port.write(command + "\r\n")
 
     def get_azimuth(self):
-        '''Gets azimuth direction of the antenna'''
+        '''Gets azimuth parameter of the antenna'''
         self._send_cmd(self.GET_AZIMUTH) # Sends the request to the Rotor
 
         azimuth = self._port.readline().strip() # Reads the response from the Rotor
         return  azimuth
 
+    def get_elevation(self):
+        '''Gets elevation parameter of the antenna'''
+        self._send_cmd(self.GET_ELEVATION)  # Sends the request to the Rotor
+
+        elevation = self._port.readline().strip()   # Reads the response from the Rotor
+        return elevation
+
+    def rotate(self, azimuth, elevaation):
+        '''Rotates antenna to the given azimuth and elevation'''
+        if azimuth < 0 or azimuth > 361:
+            return
+
+        if elevaation < 0 or elevaation > 90:
+            return
+
+        command = self.MOVE_TO + ' ' + azimuth + ' ' + elevaation
+        self._send_cmd(command)
+
+
+if __name__ == "__main__":
+    r = Rotor("COM4")
+
+    print(r.get_elevation())
+    print(r.get_azimuth())
+    r.rotate(180, 44)
