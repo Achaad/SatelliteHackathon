59660a0f5c2de2c0592cc0606e40146e405343e9
Achaad <perant@zoho.com>
2018-10-06 19:49:49 +0300

    Something worked

--- src/antenna_rotation_node.py
+++ src/antenna_rotation_node.py
@@ -13,32 +13,31 @@ class Rotor:
     def __init__(self, com_port):
         class_name = self.__class__.__name__
         # opens serial port for communication with Yaesu G-5500
-        self._port = serial.Serial(com_port, self.BAUD_RATE, stopbits=1, timeout=1, xonxoff=0, rtscts=0)
+        self._port = serial.Serial(com_port, self.BAUD_RATE, bytesize=8, stopbits=1, timeout=1, xonxoff=0, rtscts=0)
 
 
 
-    def __delete__(self, instance):
+
+    def __delete__(self):
         self._port.close()
 
     def _send_cmd(self, command):
         '''Sends command to Rotor'''
-        self._port.write((command + "\r\n").encode())
-        print(command + " executed")
+        self._port.write((command + "\r").encode())
 
     def get_azimuth(self):
         '''Gets azimuth parameter of the antenna'''
         self._send_cmd(self.GET_AZIMUTH) # Sends the request to the Rotor
 
-        print("Command goes further")
         azimuth = self._port.readline().strip() # Reads the response from the Rotor
         print("Azimuth received")
-        return  azimuth.decode()
+        return  azimuth
 
     def get_elevation(self):
         '''Gets elevation parameter of the antenna'''
         self._send_cmd(self.GET_ELEVATION)  # Sends the request to the Rotor
 
-        elevation = self._port.readline().strip()   # Reads the response from the Rotor
+        elevation = self._port.readline().strip()  # Reads the response from the Rotor
         return elevation
 
     def rotate(self, azimuth, elevation):
@@ -57,5 +56,5 @@ if __name__ == "__main__":
     r = Rotor("COM4")
 
     print(r.get_azimuth())
-    #print(r.get_elevation())
-    #r.rotate(180, 44)
+    print(r.get_elevation())
+    r.rotate(180, 44)
