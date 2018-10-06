6b781b33795ca3cd1b1ad54b270e4f3fc8a71eeb
shanyu <shanyu@ttu.ee>
2018-10-07 01:12:22 +0300

    Clean up & check the position every second

--- src/antenna_rotation_node.py
+++ src/antenna_rotation_node.py
@@ -1,5 +1,7 @@
 import serial
 import logging
+import os
+import datetime
 
 class Rotor:
     BAUD_RATE = 9600
@@ -16,7 +18,7 @@ class Rotor:
         class_name = self.__class__.__name__
         # opens serial port for communication with Yaesu G-5500
         self._port = serial.Serial(com_port, self.BAUD_RATE, bytesize=8, stopbits=1, timeout=0.5, xonxoff=0, rtscts=0)
-        logging.basicConfig(filename="/output/rotor.log")
+        logging.basicConfig(filename=(os.path.abspath(os.pardir) + "/output/rotor.log"), level=logging.DEBUG)
         self._log = logging.getLogger(name="main." + str(class_name))
 
     def __delete__(self):
@@ -25,7 +27,7 @@ class Rotor:
     def _send_cmd(self, command):
         '''Sends command to Rotor'''
         self._port.write((command + "\r").encode())
-        self._log.log("Executed command: " + command)
+        self._log.info(str(datetime.datetime.now()) + ": Executed command: " + command)
 
     def get_azimuth(self):
         '''Gets azimuth parameter of the antenna'''
@@ -33,7 +35,7 @@ class Rotor:
 
         azimuth = self._port.readline().strip() # Reads the response from the Rotor
         azimuth = int(azimuth[3:6])
-        self._log.log("Azimuth = " + azimuth)
+        self._log.info(str(datetime.datetime.now()) + ": Azimuth = " + str(azimuth))
         return  azimuth
 
     def get_elevation(self):
@@ -42,17 +44,17 @@ class Rotor:
 
         elevation = self._port.readline().strip()  # Reads the response from the Rotor
         elevation = int(elevation[3:6])
-        self._log.log("Elevation = " + elevation)
+        self._log.info(str(datetime.datetime.now()) + ": Elevation = " + str(elevation))
         return elevation
 
     def rotate(self, azimuth, elevation):
         '''Rotates antenna to the given azimuth and elevation'''
         if azimuth < 0 or azimuth > 361:
-            self._log.warning("Azimuth outside of range: " + azimuth)
+            self._log.warning(str(datetime.datetime.now()) + ": Azimuth outside of range: " + str(azimuth))
             return
 
         if elevation < 0 or elevation > 90:
-            self._log.warning("Elevation outside of range: " + elevation)
+            self._log.warning(str(datetime.datetime.now()) + ": Elevation outside of range: " + str(elevation))
             return
 
         command = self.MOVE_TO + ' ' + str(azimuth) + ' ' + str(elevation)
@@ -60,6 +62,7 @@ class Rotor:
 
 
 if __name__ == "__main__":
+    # Just an example of the use of this class
     r = Rotor("COM4")
 
     print(r.get_azimuth())
--- src/tle2signal.py
+++ src/tle2signal.py
@@ -1,53 +1,67 @@
 import ephem
 import math
+import schedule
+import time
 
 class TLEConverter:
     def __init__(self):
         self.GROUND_STATION_LATITUDE = '59.394870'
         self.GROUND_STATION_LONGITUDE = '24.661399'
         self.GROUND_STATION_ELEVATION = 1
-        self.MIN_SATELLITE_ELEVATION = 0
 
-    def convert_to_signal(self, time):
+    def run_program(self):
+        """
+        Check the Satellite position every second
+        """
+
+        schedule.every(1).seconds.do(self.get_signal)
+        while True:
+            schedule.run_pending()
+            time.sleep(0.5)
+
+    def convert_tuple_to_signal(self, tuple):
         """
         Converts the angle to a range of 0 to pi.
-        :param radians: Initial angle
+        :param: Tuple of (Azimuth, Altitude)
         """
-        lst = str(time).split(':')
-        return float(lst[0]) + float(lst[1]) / 60.0 + float(lst[2]) / 3600.0
+
+        def str2float(strlst):
+            if float(strlst[0]) < 0:
+                return float(strlst[0]) - float(strlst[1]) / 60.0 - float(strlst[2]) / 3600.0
+            return float(strlst[0]) + float(strlst[1]) / 60.0 + float(strlst[2]) / 3600.0
+
+        lstAz, lstEle = str(tuple[0]).split(':'), str(tuple[1]).split(':')
+
+        return (str2float(lstAz), str2float(lstEle))
 
     def get_signal(self):
-        final_max_elev_time = ""
-        final_max_elev = 0
+        """
+        Take TLE and compute the azimuth & elevation
+        """
+        
         tle_lines = []
+
         for line in open('E:\\Course\\TTU\\CubeSat\\parabol-control-development\\src\\antenna\\antenna_controller\\tle\\tle.txt', 'r').readlines():
             tle_lines.append(line)
-        print("Next satellite TLE: \n{}".format(tle_lines))
-        print("")
+        # print("Next satellite TLE: \n{}".format(tle_lines))
 
-        obs = ephem.Observer()
-        obs.lat = self.GROUND_STATION_LATITUDE
-        obs.long = self.GROUND_STATION_LONGITUDE
-        obs.elevation = self.GROUND_STATION_ELEVATION
+        observer = ephem.Observer()
+        observer.lat = self.GROUND_STATION_LATITUDE
+        observer.long = self.GROUND_STATION_LONGITUDE
+        observer.elev = self.GROUND_STATION_ELEVATION
  
-        print(obs)
-        print("")
-
         satellite = ephem.readtle(tle_lines[0], tle_lines[1], tle_lines[2])
-        satellite.compute(obs)
-        print(satellite)
-        print("")
+        satellite.compute(observer)
 
-        rise_time, rise_az, max_elev_time, max_elev, set_time, set_az = obs.next_pass(satellite)
-        print("yoo")
-        print("rise_time: ", rise_time, " rise_az: ", rise_az, " max_elev_time: ", max_elev_time, "max_elev: ",  max_elev, "set_time: ", set_time, "set_az: ", set_az)
-
-        print("AZ ", satellite.az, "AZ.convert ", self.convert_to_signal(satellite.az),"ELE ", satellite.alt, "ELE.conver ", self.convert_to_signal(satellite.alt))
-        return (self.convert_to_signal(satellite.az), self.convert_to_signal(satellite.alt))
+        # print(self.convert_to_signal(satellite.az), self.convert_to_signal(satellite.alt))
+        print(self.convert_tuple_to_signal((satellite.az, satellite.alt)))
+        # return (self.convert_to_signal(satellite.az), self.convert_to_signal(satellite.alt))
+        return self.convert_tuple_to_signal((satellite.az, satellite.alt))
 
 def main():
     converter = TLEConverter()
-    converter.get_signal()
+    converter.run_program()
+
 
 if __name__ == '__main__':
     main()
