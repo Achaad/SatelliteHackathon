b3aa5b2f178d432f6cbd8d58ba05f9df3bae3cd9
Achaad <perant@zoho.com>
2018-10-06 17:07:59 +0300

    Added destructor to class

--- src/antenna_rotation_node.py
+++ src/antenna_rotation_node.py
@@ -9,4 +9,9 @@ class Rotor:
         class_name = self.__class__.__name__
         # opens serial port for communication with Yaesu G-5500
         self._port = serial.Serial(com_port, self.BAUD_RATE, stopbits=1, timeout=None, xonxoff=0, rtscts=0)
-        
+
+
+    def __delete__(self, instance):
+        self._port.close()
+
+    
