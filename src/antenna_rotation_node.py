5aacc983ab369eb79cc0a9a553b386bd8bbdc71a
Achaad <perant@zoho.com>
2018-10-06 17:22:11 +0300

    Added auxillary method and getter for azimuth

--- src/antenna_rotation_node.py
+++ src/antenna_rotation_node.py
@@ -3,6 +3,11 @@ import serial
 class Rotor:
     BAUD_RATE = 9600
 
+    # List of available commands
+    GET_ELEVATION = 'B'
+    GET_AZIMUTH = 'C'
+    MOVE_TO = 'W'
+
     _port = None
 
     def __init__(self, com_port):
@@ -14,4 +19,14 @@ class Rotor:
     def __delete__(self, instance):
         self._port.close()
 
-    
+    def _send_cmd(self, command):
+        '''Sends command to Rotor'''
+        self._port.write(command + "\r\n")
+
+    def get_azimuth(self):
+        '''Gets azimuth direction of the antenna'''
+        self._send_cmd(self.GET_AZIMUTH) # Sends the request to the Rotor
+
+        azimuth = self._port.readline().strip() # Reads the response from the Rotor
+        return  azimuth
+
