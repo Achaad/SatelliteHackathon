ee2846c5f9a4b23cf80876a43ee7c4a87c47990d
kamatt <kamatt@ttu.ee>
2018-10-07 12:27:54 +0300

    improved comments and functions

--- src/antenna_tracker_node.py
+++ src/antenna_tracker_node.py
@@ -9,26 +9,45 @@ class Tracker:
     # Get longitude, latitude and altitude of the satellite
     print("Longitude, latitude and altitude")
     print(orb.get_lonlatalt(now))
+    # My position
+    my_lat = '59.394891599999994'
+    my_long = '24.660768899999997'
 
     def __init__(self):
         # class_name = self.__class__.__name__
-        self.tracking_the_satellite(self.waiting_for_the_satellite)
+        self.waiting_for_the_satellite()
+        self.tracking_the_satellite()
+        """
+        Make these functions continuous
+        """
+
 
     def waiting_for_the_satellite(self):
-        # While loop
-        # Convert orb.get_lonlatalt(now) to longitude and latitude
+
         """
-        While loop runs until the satellite
-        is in suitable position to track.
-        :return: longitude, latitude
+        While loop
+        If satellite reaches to our range then break the loop
+        :return:
         """
 
-    def tracking_the_satellite(self, longitude, latitude):
+    def where_to_turn(self):
+
+        """
+        orb.get_lonlatalt(now) what gives us
+        longitude, latitude and altitude of the satellite
+        calculating where to turn
+        and returning azimuth and elevation
+        :return: azimuth, elevation
+        """
+
+
+    def tracking_the_satellite(self):
+        # Turn to
         # While the satellite is in our range then track
 
         """
         Rotate in while loop until it flies out of our range.
-        :param longitude:
-        :param latitude:
+        :param azimuth:
+        :param elevation:
         :return:
         """
